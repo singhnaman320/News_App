92e7a66b5ae6e73976c09a0bca0fb6cd44ba8578
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-30 18:04:18 +0530

    Modifying Spinner.js

--- newsapp/src/components/Spinner.js
+++ newsapp/src/components/Spinner.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
 import loading from './loading.gif'
 
-export class Spinner extends Component {
-  render() {
+const Spinner = () => {
     return (
       <div className='text-center'>
         <img  id='spinner' className='my-3' src={loading} alt="loading here" />
       </div>
     )
-  }
 }
 export default Spinner
