bc01229412e635174895d930babd3e937cf7136f
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-30 18:08:58 +0530

    Modifying NewsItem.js

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-const NewsItem = () =>{
+const NewsItem = (props) =>{
 
-    let{title, description, imageUrl, newsUrl, author, date, source} = this.props; 
+    let{title, description, imageUrl, newsUrl, author, date, source} = props; 
     return (
       <div className='my-3'>
         <div className="card">
