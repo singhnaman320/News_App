7a61b8eb1f85779cf6f5b8d4b11836f2f03fc7d9
Naman Kumar Singh <singhnaman320@gmail.com>
2023-06-30 17:33:17 +0530

    Adding News.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -95,11 +95,11 @@ export class News extends Component {
             <div className="row">
               {this.state.articles.map((element) => {
 
-                  return <div className="col-md-4" key={element.url}> 
+                return <div className="col-md-4" key={element.url}> 
         
-                      <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} 
-                      imageUrl={element.urlToImage} newsUrl={element.url} author={element.author ? element.author: 'Vikram Chandra'} date={element.publishedAt} source={element.source.name}/>
-                  </div>
+                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} 
+                    imageUrl={element.urlToImage} newsUrl={element.url} author={element.author ? element.author: 'Vikram Chandra'} date={element.publishedAt} source={element.source.name}/>
+                </div>
               })}
             </div>
           </div>
